6377928f473d90e42ce704d9a0d59e5b1ac81ae2
Furkan Ertantu <furkanertantu@outlook.com>
2023-09-12 17:15:40 +0300

    a static data for the api tests

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using bookDemo.Models;
+
+namespace bookDemo.Data
+{
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book(){Id=1, Title="Hamlet", Price = 75},
+                new Book(){Id=2, Title="Faust", Price = 150},
+                new Book(){Id=3, Title="Frankestain", Price = 75}
+            };
+        }
+    }
+}
